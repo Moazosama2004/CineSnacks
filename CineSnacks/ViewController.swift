29ec2eaa96b63f3984ed755af00a8a299545e4a2
Moaz Osama <moazosama204@gmail.com>
2026-05-29 15:58:05 -0700

    Initial Commit

---
+++ CineSnacks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineSnacks
+//
+//  Created by Moaz on 29/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
