bd2a8ab26583406d572a5e56491eaf2f51ec8906
Moaz Osama <moazosama204@gmail.com>
2026-05-29 19:32:14 -0700

    add init home view controller with nav controller

---
+++ CineSnacks/Views/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  CineSnacks
+//
+//  Created by Moaz on 29/05/2026.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
